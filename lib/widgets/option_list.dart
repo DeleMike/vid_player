1b948aa106f834c2c836c8823bdb89b6e5f5c469
DeleMike <akindelemichael65@gmail.com>
2021-01-21 12:35:53 +0100

    add bottom sheet options

--- lib/widgets/option_list.dart
+++ lib/widgets/option_list.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import '../screens/video_screen.dart';
+
 class OptionList extends StatelessWidget {
+  final Future<File> videoFile;
+  final String videoTitle;
+  OptionList({
+    @required this.videoFile,
+    @required this.videoTitle,
+  });
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
@@ -12,20 +22,51 @@ class OptionList extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             FlatButton(
-              child: Text('Play'),
-              onPressed: () {},
+              splashColor: Theme.of(context).splashColor,
+              child: Text(
+                'Play',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                 Navigator.of(context).pushNamed(
+                        VideoScreen.routeName,
+                        arguments: {
+                          'video-title': videoTitle,
+                          'video-file': videoFile,
+                        },
+                      );
+              },
             ),
             FlatButton(
-              child: Text('Video information'),
-              onPressed: () {},
+              splashColor: Theme.of(context).splashColor,
+              child: Text(
+                'Video information',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
             FlatButton(
-              child: Text('Add subtitle'),
-              onPressed: () {},
+              splashColor: Theme.of(context).splashColor,
+              child: Text(
+                'Add subtitle',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
             FlatButton(
-              child: Text('Play as audio'),
-              onPressed: () {},
+              splashColor: Theme.of(context).splashColor,
+              child: Text(
+                'Play as audio',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
